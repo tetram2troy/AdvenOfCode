0fa45e7e0a279913e0dbda80056e0dd29bdde2a9
Fabrice Lichtlin <tetram2troy@gmail.com>
2022-12-01 23:20:01 -0500

    [2022] first day

---
+++ 2022/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+
+Console.WriteLine("read csv ");
+
+using (var reader = new StreamReader("input.csv"))
+{
+    var sumList = new List<int> ();
+    var sum = 0;
+    while (! reader.EndOfStream)
+    {
+        var line = reader.ReadLine();
+        if (string.IsNullOrEmpty(line)) 
+        {
+            sumList.Add(sum);
+            sum = 0;
+        }
+        else
+        {
+            int lineParsed = Int32.Parse(line);
+            sum += lineParsed;
+        }
+    }
+    var result = sumList.OrderDescending().Take(3).Sum();
+
+    Console.WriteLine($"max : {result}");
+}
---
+++ 2022/ProgramExo1.cs
@@ -0,0 +1,27 @@
+﻿// using System.IO;
+
+
+// Console.WriteLine("read csv ");
+
+// using (var reader = new StreamReader("input.csv"))
+// {
+//     var max = 0;
+//     var sum = 0;
+//     while (! reader.EndOfStream)
+//     {
+//         var line = reader.ReadLine();
+//         if (string.IsNullOrEmpty(line)) 
+//         {
+//             if (sum > max) max = sum;
+//             sum = 0;
+//         }
+//         else
+//         {
+//             int lineParsed = Int32.Parse(line);
+//             sum += lineParsed;
+//         }
+
+//     }
+
+//     Console.WriteLine($"max : {max}");
+// }
